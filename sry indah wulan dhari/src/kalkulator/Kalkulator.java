5639181e09f2f8f1d5d2acee7c639d13aa158c04
sryindahwulandhari <98534088+sryindahwulandhari@users.noreply.github.com>
2022-01-31 08:59:44 +0700

    Add files via upload

---
+++ sry indah wulan dhari/src/kalkulator/Kalkulator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulator;
+import kalkulator.Kalkulator1;
+/**
+ *
+ * @author ASUS
+ */
+public class Kalkulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Kalkulator1().setVisible(true);
+    }
+
+    static void getData() {
+        
+    }
+    
+    
+    
+}
